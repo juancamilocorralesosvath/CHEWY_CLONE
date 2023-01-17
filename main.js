29fc0619dc541d87ddce8ea815a7d7c795305ade
luisfegz <luisfelipegaitanzambrano@gmail.com>
2023-01-16 19:20:30 -0500

    Noveno Commit

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 // alert("hello world!")
+let isActive = false;
+const hambMenu = document.getElementById('hamb-menu');
+const displayList = document.getElementById('display-list');
+hambMenu.addEventListener("click", ()=>{
+    
+    if (isActive){
+        //displayList.style.display="none";
+        displayList.style.opacity="1";
+        isActive = false;
+    }
+    else{
+        //displayList.style.display="flex";
+        displayList.style.opacity="0";
+        isActive = true;
+    }
+    
+})
+
+
+console.log("Hola");
